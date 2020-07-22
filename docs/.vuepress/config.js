6dab31ef67081f5fe0765cae260d25623717e55e
101dal <66965262+101dal@users.noreply.github.com>
2020-07-22 15:18:13 +0200

    THEME

--- docs/.vuepress/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around'
-  }
-
-module.exports = {
-  theme: '@org/vuepress-theme-xxx', // or an official theme: '@vuepress/theme-xxx'
-}

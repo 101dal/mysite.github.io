cbde0d76415cffde45e2be2562f1811255b5a25f
101dal <66965262+101dal@users.noreply.github.com>
2020-07-27 13:26:59 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,11 +2,20 @@ module.exports = {
   themeConfig: {
     logo: './img/logo.png',
     themeConfig: {
+      navbar: true,
       nav: [
         { text: 'Home', link: '/' },
         { text: `Server's informations`, link: '/en/infos/serverinfo/' },
         { text: `Website's informations`, link: '/en/infos/websiteinfo/' },
         { text: 'Github', link: 'https://github.com/101dal/mysite.github.io' }
+        {
+          text: 'Languages',
+          ariaLabel: 'Language Menu',
+          items: [
+            { text: 'English', link: '/en/' },
+            { text: 'French', link: '/fr/' }
+          ]
+        }
       ]
     }
   }

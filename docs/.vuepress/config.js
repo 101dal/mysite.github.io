9e62de46903d2a5eae6edc0a12b39b7f5e902708
101dal <66965262+101dal@users.noreply.github.com>
2020-07-27 10:52:40 +0200

    22

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // D'autres éléments de configuration
+    theme: 'vuepress-theme-gouv-fr',
+    themeConfig: {
+        sidebarTitles: {
+            '/en/infos/serverinfos/': "Server infos",
+            '/en/infos/websiteinfo/': "Website infos",
+        }
+      }
+    }

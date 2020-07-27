ac8c2775a9775b4533eca6d19ca74f5e209cb90c
101dal <66965262+101dal@users.noreply.github.com>
2020-07-27 13:16:37 +0200

    27

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,13 @@
 module.exports = {
   themeConfig: {
     logo: './img/logo.png',
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: `Server's informations`, link: '/en/infos/serverinfo/' },
+        { text: `Website's informations`, link: '/en/infos/websiteinfo/' },
+        { text: 'Github', link: 'https://github.com/101dal/mysite.github.io' }
+      ]
+    }
   }
 }

ee36a5f5cf9ae81c11e61273369f28bf94ae79bb
101dal <66965262+101dal@users.noreply.github.com>
2020-07-27 13:38:56 +0200

    28

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,23 @@
 module.exports = {
   themeConfig: {
     logo: './img/logo.png',
+    theme: 'vuepress-theme-gouv-fr',
     themeConfig: {
-      navbar: true,
-      nav: [
-        { text: 'Home', link: '/' },
-        { text: `Server's informations`, link: '/en/infos/serverinfo/' },
-        { text: `Website's informations`, link: '/en/infos/websiteinfo/' },
-        { text: 'Github', link: 'https://github.com/101dal/mysite.github.io' }
-        {
-          text: 'Languages',
-          ariaLabel: 'Language Menu',
-          items: [
-            { text: 'English', link: '/en/' },
-            { text: 'French', link: '/fr/' }
-          ]
-        }
-      ]
+      '/fr/' : {navbar: true,
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: `Server's informations`, link: '/en/infos/serverinfo/' },
+          { text: `Website's informations`, link: '/en/infos/websiteinfo/' },
+          { text: 'Github', link: 'https://github.com/101dal/mysite.github.io' }
+          {
+            text: 'Languages',
+            ariaLabel: 'Language Menu',
+            items: [
+              { text: 'English', link: '/en/' },
+              { text: 'French', link: '/fr/' }
+            ]
+          }
+        ]}
     }
   }
 }
